48c035345b72df09fb24a5ab33909f8c00b719e9
Kenneth Walker <kwalkerdev@gmail.com>
2021-04-28 14:57:32 -0700

    Further refined unit tests.

--- poker_deck/src/main.cpp
+++ poker_deck/src/main.cpp
@@ -4,29 +4,54 @@
 int main()
 {
 	Poker::Deck deck;
+
 	std::cout << "Starting Deck (size = " << deck.size() << "):\n";
-	deck.print(); std::cout << "\n";
+	deck.print();
+
 	deck.shuffle();
-	std::cout << "Shuffled Deck (size = " << deck.size() << "):\n";
-	deck.print(); std::cout << "\n";
-	auto dealt_cards = deck.deal_cards(52);
-	for (const auto& dealt_card : dealt_cards)
+	std::cout << "\nShuffled Deck (size = " << deck.size() << "):\n";
+	deck.print();
+
+	std::cout << "\nDealing 52 cards to Hand...\n";
+	auto hand = deck.deal_cards(52);//Executes 52 calls to deal_card function on this deck.
+	std::cout << "Hand (size = " << hand.size() << "):\n";
+	for (const auto& card : hand)
 	{
-		if(dealt_card)
-			std::cout << "Dealt Card: " << *dealt_card << "\n";
+		if(card)
+			std::cout << *card << "\n";
 	}
-	std::cout << "\n";
-	std::cout << "Deck After Dealing Cards (size = " << deck.size() << "):\n";
-	deck.print(); std::cout << "\n";
-	auto dealt_card = deck.deal_card();
+
+	std::cout << "\nDeck After Dealing Cards (size = " << deck.size() << "):\n";
+	deck.print();
+
+	std::cout << "\nDealing 1 card to Hand.\n";
+	auto dealt_card = deck.deal_card();//Executes the 53rd call to deal_card function on this deck.
+	
 	if (dealt_card)
 	{
-		std::cout << "Dealt Card: " << *dealt_card << "\n";
+		hand.push_back(std::move(dealt_card));
+		std::cout << "Hand (size = " << hand.size() << "):\n";
+		for (const auto& card : hand)
+		{
+			if (card)
+				std::cout << *card << "\n";
+		}
 	}
 	else
 	{
 		std::cout << "No more cards left in deck.\n";
 	}
+
+	//deck.replace_cards(hand);
+	//std::cout << "\nDeck After Replacing Cards (size = " << deck.size() << "):\n"; 
+	//deck.print();
+
+	//std::cout << "\nHand (size = " << hand.size() << "):\n";
+	//for (const auto& dealt_card : hand)
+	//{
+	//	if (dealt_card)
+	//		std::cout << "Dealt Card: " << *dealt_card << "\n";
+	//}
 
 	return 0;
 }
